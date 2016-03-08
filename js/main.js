f1f68e548cbb42f7ceb548647b0cabb0b45c82a6
oliviaarden <mac99@SRMAC02.local>
2016-03-08 17:35:21 -0500

    code in js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$('header').addClass('masthead');
+$('h1').removeClass('big-heading');
+$('main').addClass('wrapper');
+$('p:first-child').addClass('intro');
+$('h2').addClass('slug-head');
+$('ul').addClass('slug-list');
+$('img').addClass('slug-img');
+$('ul li:first-child').addClass('slug-list-first');
+$('ul li:last-child').addClass('slug-list-last');
+$('dl').addClass('classification');
+$('dt').removeClass('big-label');
+$('ul li:last-child').show();
