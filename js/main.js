874d3aa3184df29e8fb6eb9543dca08ed8a1a4c0
oliviaarden <mac99@SRMAC02.local>
2016-03-08 17:43:12 -0500

    fixed js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,35 @@
-$('header').addClass('masthead');
-$('h1').removeClass('big-heading');
-$('main').addClass('wrapper');
-$('p:first-child').addClass('intro');
-$('h2').addClass('slug-head');
-$('ul').addClass('slug-list');
-$('img').addClass('slug-img');
-$('ul li:first-child').addClass('slug-list-first');
-$('ul li:last-child').addClass('slug-list-last');
-$('dl').addClass('classification');
-$('dt').removeClass('big-label');
-$('ul li:last-child').show();
+var $header = $('<header>');
+$header .addClass(".masthead");
+
+var $h1 = $('<h1>');
+$h1.removeClass(".big-heading") .addClass('.logo');
+
+var $main = $('<main>');
+$header .addClass(".wrapper");
+
+var $p = $('<p="first-child">');
+$p.hasClass("<main>") .addClass ('.slug-list');
+
+var $h2 = $('<h2>');
+$h2 .addClass(".slug-head");
+
+var $h2 = $('<h2>');
+$h2 .addClass(".slug-head");
+
+var $main = $('<main>');
+$main .addClass(".slug-list");
+
+var $li = $('<li="first-child>');
+$li .addClass(".slug-list-first");
+
+var $li = $('<li="last-child>');
+$li .addClass(".slug-list-last");
+
+var $dl = $('<dl>');
+$dl .addClass(".classification");
+
+var $dt = $('<dt>');
+$dt .removeClass(".big-lable");
+
+var $li = $('<li="last-child>');
+$li .show();
